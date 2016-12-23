3ae4ceff1a56490e4bfe6ece943201d232a2833b
Justin Sloane <jslo@rallyaway.com>
2016-12-22 21:32:02 -0500

    Added more javascript to link images to their appropriate galleries. Eventually need to either create pages for each gallery or figure out how to cycle through images on the page. Probably a loop with an array.

--- Scripts/pastTripsScript.js
+++ Scripts/pastTripsScript.js
@@ -1,12 +1,27 @@
+ // class="hvr-ripple-in"> DON'T FORGET TO ADD THIS EFFECT BACK IN
+ // ALSO NEED TO FIGURE OUT HOW TO GET MOUSE TO CHANGE WHEN HOVERING ON LINK VIA JAVA
+
+
+
 document.getElementById("southwest").onmouseover = function(){
 	document.getElementById("southwest").src = "Images/Zion.png";
 
 }
 
+	document.getElementById("southwest").onclick = function(){
+		window.open("http://www.rallyaway.com/gallery/southwest");
+
+	}
+
 document.getElementById("northwest").onmouseover = function(){
 	document.getElementById("northwest").src = "Images/Olympic.png";
 
 }
+
+	document.getElementById("northwest").onclick = function(){
+		window.open("http://www.rallyaway.com/gallery/pnw");
+
+	}
 
 
 document.getElementById("barntime").onmouseover = function(){
@@ -14,20 +29,38 @@ document.getElementById("barntime").onmouseover = function(){
 
 }
 
+	document.getElementById("barntime").onclick = function(){
+		window.open("http://www.rallyaway.com/gallery/barntime");
+
+	}
+
 
 document.getElementById("roadtrip").onmouseover = function(){
 	document.getElementById("roadtrip").src = "Images/RoadTrip3.png";
 
 }
 
+	document.getElementById("roadtrip").onclick = function(){
+		window.open("http://www.rallyaway.com/gallery/9000miles");
+
+	}
 
 document.getElementById("firstRally").onmouseover = function(){
 	document.getElementById("firstRally").src = "Images/Beacon3.png";
 
 }
 
+	document.getElementById("firstRally").onclick = function(){
+		window.open("http://www.rallyaway.com/gallery/rallybeacon");
+
+	}
 
 document.getElementById("newport").onmouseover = function(){
 	document.getElementById("newport").src = "Images/Newport2.png";
 
 }
+
+	document.getElementById("newport").onclick = function(){
+		window.open("http://www.rallyaway.com/gallery/newport16");
+
+	}
