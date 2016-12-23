541df23aa99d171e70f6fc3b2ad3c42f6d410ba3
Justin Sloane <jslo@rallyaway.com>
2016-12-22 21:19:48 -0500

    First javascript added! On Good Times, made it so that the pics for each trip changes when you scroll over. Unfortunately this affects the links. I also want to figure out how to have it switch back or cycle through. If i can do that, then I don't have to link it and can just have someone click through a bunch of pictures within each square.

---
+++ Scripts/pastTripsScript.js
@@ -0,0 +1,33 @@
+document.getElementById("southwest").onmouseover = function(){
+	document.getElementById("southwest").src = "Images/Zion.png";
+
+}
+
+document.getElementById("northwest").onmouseover = function(){
+	document.getElementById("northwest").src = "Images/Olympic.png";
+
+}
+
+
+document.getElementById("barntime").onmouseover = function(){
+	document.getElementById("barntime").src = "Images/Barn2.png";
+
+}
+
+
+document.getElementById("roadtrip").onmouseover = function(){
+	document.getElementById("roadtrip").src = "Images/RoadTrip3.png";
+
+}
+
+
+document.getElementById("firstRally").onmouseover = function(){
+	document.getElementById("firstRally").src = "Images/Beacon3.png";
+
+}
+
+
+document.getElementById("newport").onmouseover = function(){
+	document.getElementById("newport").src = "Images/Newport2.png";
+
+}
